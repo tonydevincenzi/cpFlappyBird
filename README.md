a341e597542db30790bfcdd7bb51e4fae0b3ff09
Anthony DeVincenzi <tonydev@gmail.com>
2016-03-26 21:25:10 -0700

    Project init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Week Eight - Flappy Bird Demo
+
+ UIKit Dynamics basics.
+
+Time spent: 2 hours
+
+![Video Walkthrough](flappy.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
--- cpFlappyBird/ViewController.swift
+++ cpFlappyBird/ViewController.swift
@@ -8,18 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    
+    @IBOutlet weak var bird: UIImageView!
+    @IBOutlet weak var birdContainer: UIView!
+    
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var push: UIPushBehavior!
+    var collision: UICollisionBehavior!
+    var timer: NSTimer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity = UIGravityBehavior()
+        gravity.magnitude = 1
+        animator.addBehavior(gravity)
+
+        push = UIPushBehavior()
+        push = UIPushBehavior(items: [self.birdContainer], mode: .Instantaneous)
+        push.pushDirection = CGVectorMake(0.0, -0.5);
+        animator.addBehavior(push)
+
+        gravity.addItem(birdContainer)
+        
+        collision = UICollisionBehavior()
+        collision.collisionDelegate = self
+        animator.addBehavior(collision)
+        
+        collision.addItem(birdContainer)
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: "addPipes", userInfo: nil, repeats: true)
+        addPipes()
+        
+        let tap = UITapGestureRecognizer(target: self, action:Selector("didTapView:"))
+        self.view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func addPipes() {
+        
+        let topPipe = UIImageView(image: UIImage(named: "pipeTop.png"))
+        let pipeY = arc4random_uniform(150)
+        topPipe.frame = CGRect(x: view.bounds.width, y: -CGFloat(pipeY), width: 52, height: 320)
+        view.addSubview(topPipe)
+        
+        let bottomPipeOffset = arc4random_uniform(50)
+        
+        let bottomPipe = UIImageView(image: UIImage(named: "pipeBottom.png"))
+        bottomPipe.frame = CGRect(x: view.bounds.width, y: topPipe.frame.height + 50 + CGFloat(bottomPipeOffset), width: 52, height: 320)
+        view.addSubview(bottomPipe)
+        
+        let pipeBehavior: UIDynamicItemBehavior = UIDynamicItemBehavior(items: [topPipe, bottomPipe])
+        pipeBehavior.addLinearVelocity(CGPoint(x: -70,y: 0), forItem: topPipe)
+        pipeBehavior.addLinearVelocity(CGPoint(x: -70,y: 0), forItem: bottomPipe)
 
+        pipeBehavior.friction = 0
+        pipeBehavior.resistance = 0
+        collision.addItem(topPipe)
+        collision.addItem(bottomPipe)
+        animator.addBehavior(pipeBehavior)
+        
+    }
+
+    @IBAction func didTapView(sender: UITapGestureRecognizer) {
+        
+        push.active = false
+        push.addItem(birdContainer)
+        push.active = true
+        
+    }
+    
+    func collisionBehavior(behavior: UICollisionBehavior, beganContactForItem item1: UIDynamicItem, withItem item2: UIDynamicItem, atPoint p: CGPoint) {
+        let view1 = item1 as! UIView
+        let view2 = item2 as! UIView
+        
+       print("Game over.")
+        animator.removeAllBehaviors()
+        timer.invalidate()
+    }
 
 }
 
